7a07cc747f82d71b80bc31ad24262bbfe4ce0a76
Kai391 <k391walkerkrishna@gmail.com>
2021-10-11 22:48:18 +0530

    Backend

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ mongodb();
 const express = require('express')
 const app = express()
 const cors = require("cors");
-const port = 8001
+const port = 3001
 
 app.use(express.json());
 app.use(cors());
