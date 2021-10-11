abf9765d974a4d072279e2da961710968252c232
Kai391 <k391walkerkrishna@gmail.com>
2021-10-11 22:44:12 +0530

    Backend

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(cors());
 
 // only for test
 app.get('/',(req,res)=>{
+  console.log("Request is made on /");
   res.status(200).json({
     status:"Ok",
     msg:"Application is working properly"
