612bead896b773c3c551dab0686535438a44fd5a
Krishna Murti Dubey <63901003+Kai391@users.noreply.github.com>
2021-10-11 22:51:01 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function main() {
-  await mongoose.connect('mongodb+srv://kai:lkzjh1sIUdsFkgmw@cluster0.qnk25.mongodb.net/iNoteBookDB?retryWrites=true&w=majority',()=>{
+  await mongoose.connect('mongodb+srv://kai:lkzjh1sIUdsFkgmw@cluster0.qnk25.mongodb.net/iNoteBookDB?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true },()=>{
       console.log("connected to db");
   });
 }
